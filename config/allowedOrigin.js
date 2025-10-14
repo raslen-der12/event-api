1b0a57fdd8a9d1ae9ccf6dfcf45582c31ae098e4
root <root@srv1060717.hstgr.cloud>
2025-10-14 10:50:33 +0000

    v1.0.0

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,10 +1,15 @@
-
 require('dotenv').config({ path: '../.env' });
 
-
-
 const allowedOrigins = [
-    'http://localhost:3000',
-]
+  'http://eventra.cloud',
+  'https://eventra.cloud',
+  'http://www.eventra.cloud',
+  'https://www.eventra.cloud',
+  'http://api.eventra.cloud',
+  'https://api.eventra.cloud',
+  'http://localhost:3001',
+  'http://127.0.0.1:3001',
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
+
